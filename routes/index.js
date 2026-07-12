a6fa81eb81983f6fe276a5ef9c288e428241f88b
budi3312101075 <budiprayoga408@gmail.com>
2026-07-12 19:32:49 +0700

    add controller kamar

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
-import express from "express"
+import express from "express";
 import authRoutes from "./api/auth.js";
 import kostRoutes from "./api/kost.js";
+import kamarRoutes from "./api/kamar.js";
 
 const app = express();
 
 app.use(authRoutes);
 app.use(kostRoutes);
+app.use(kamarRoutes);
 
 export default app;
